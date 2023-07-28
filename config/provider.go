f8444168b1f7eebed33dcdd07bafb7b37878996a
Christopher Haar <christopher.haar@upbound.io>
2023-07-28 18:47:01 +0200

    first commit

---
+++ config/provider.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2021 Upbound Inc.
+*/
+
+package config
+
+import (
+	// Note(turkenh): we are importing this to embed provider schema document
+	_ "embed"
+
+	ujconfig "github.com/upbound/upjet/pkg/config"
+
+	"github.com/haarchri/provider-testhub/config/null"
+)
+
+const (
+	resourcePrefix = "testhub"
+	modulePath     = "github.com/haarchri/provider-testhub"
+)
+
+//go:embed schema.json
+var providerSchema string
+
+//go:embed provider-metadata.yaml
+var providerMetadata string
+
+// GetProvider returns provider configuration
+func GetProvider() *ujconfig.Provider {
+	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
+		ujconfig.WithRootGroup("haarchri.io"),
+		ujconfig.WithIncludeList(ExternalNameConfigured()),
+		ujconfig.WithFeaturesPackage("internal/features"),
+		ujconfig.WithDefaultResourceOptions(
+			ExternalNameConfigurations(),
+		))
+
+	for _, configure := range []func(provider *ujconfig.Provider){
+		// add custom config functions
+		null.Configure,
+	} {
+		configure(pc)
+	}
+
+	pc.ConfigureResources()
+	return pc
+}
